0f3f6172d6324e191acb87cee8b47ce7b6862a75
Serge Gusarov <mrhersergik@gmail.com>
2025-10-20 16:16:14 +0300

    shop final

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "shop"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("course/<int:course_id>/", views.course_detail, name="course_detail"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse
 from .models import Category, Course
 # Create your views here.
 
 def index(request):
-    # return render(request, "shop/index.html")
+    # return render(request, "shop/templates/index.html")
     courses = Course.objects.all()
-    return HttpResponse([str(course)+'<br/>' for course in courses])
-    # return render(request, "shop/index.html", {"courses": courses})
+    # return HttpResponse([str(course)+'<br/>' for course in courses])
+    return render(request, "shop/course.html", {"courses": courses, "title": "Courses"})
+
+def course_detail(request, course_id):
+    # try:
+    #     # course = Course.objects.get(id=course_id)
+    #     course = Course.objects.get(pk=course_id)
+    #     return render(request, "course_detail.html", {"course": course, "title": course.title})
+    # except Course.DoesNotExist:
+    #     # raise Http404("Course not found")
+    #     return HttpResponse("Course not found", status=404)
+
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, "shop/course_detail.html", {"course": course, "title": course.title})
+
+# def category_detail(request, category_id):
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, "category_detail.html", {"category": category, "title": category.title})
